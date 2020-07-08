19df7c2ad42811f09854bad105c96265064a28f1
mania_fish <mania_fish@163.com>
2020-07-08 17:05:54 +0800

    fix t92

--- t92.go
+++ t92.go
@@ -2,7 +2,6 @@ package main
 
 /*
 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
-
 说明:
 1 ≤ m ≤ n ≤ 链表长度。
 
@@ -18,43 +17,28 @@ type ListNode struct {
 }
 
 func reverseBetween(head *ListNode, m int, n int) *ListNode {
-	if n <= m {
+	if n <= m || head == nil || head.Next == nil {
 		return head
 	}
 
-	// 前段最后一个节点，中段三个反转指针，中段头一个节点
-	p, p1, p2, p3, p4 := head, head, head, head, head
-	if m > 1 { // 前面至少有一个节点保持原样
-		for i := 1; i < m-1 && p != nil; i, p = i+1, p.Next {
-		}
-
-		if p == nil || p.Next == nil || p.Next.Next == nil {
-			return head
-		}
-
-		p1, p2, p3, p4 = p.Next, p.Next.Next, p.Next, p.Next
-	} else { // 从第一位开始反转
-		if p == nil || p.Next == nil {
-			return head
-		}
-
-		p1, p2, p3, p4 = p, p.Next, p, p
-		p = nil
+	var h1 *ListNode
+	p1, p2, p3 := head, head.Next, head.Next
+	for i := 0; i < m-1 && p2 != nil; i++ {
+		h1, p1, p2, p3 = p1, p1.Next, p2.Next, p3.Next
 	}
 
 	for i := m; i < n && p2 != nil; i++ {
 		p3 = p2.Next
 		p2.Next = p1
-		p1 = p2
-		p2 = p3
+		p1, p2 = p2, p3
 	}
 
-	// 中段头(即反转后的尾)指向后段头
-	p4.Next = p3
-	if p != nil { // 有前段
-		p.Next = p1
-		return head
-	} else {
+	if h1 == nil { // 头是p1
+		head.Next = p2
 		return p1
 	}
+
+	h2 := h1.Next
+	h1.Next, h2.Next = p1, p2
+	return head
 }
